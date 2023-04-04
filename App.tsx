15930d36d01b24389b059e24b4be6fd95ac60972
Adam Nitti <adamn@sodiumhalogen.com>
2023-04-04 16:30:08 -0500

    🧹  CHORE: remove unused

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ViewStyle } from "react-native";
+import { Text, View, ViewStyle } from "react-native";
 
 export default function App() {
   const ROOT: ViewStyle = {
